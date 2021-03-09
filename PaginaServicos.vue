511ed6242d9e5eedc09395adaf30f6f514fd8a96
EVANDRO SIQUEIRA SIQUER SIS - SOLUÇÕES E SISTEMAS <evandrosiquer@hotmail.com>
2021-03-09 19:45:28 -0300

    Aulas modulo 5.

---
+++ PaginaServicos.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Serviços da Empresa</h1>
+    <ul>
+      <li>Front End</li>
+      <li>Back End</li>
+      <li>Marketing</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PaginaServicos",
+};
+</script>
---
+++ home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li @click="componenteAtivo = 'PaginaSobre'">Sobre</li>
+      <li @click="componenteAtivo = 'PaginaServicos'">Serviços</li>
+    </ul>
+    <component :is="componenteAtivo"></component>
+    <transition mode="out-in"></transition>
+  </div>
+</template>
+
+<script>
+import PaginaSobre from "./PaginaSobre.vue";
+import PaginaServicos from "./PaginaServicos.vue";
+
+export default {
+  components: {
+    PaginaSobre,
+    PaginaServicos,
+  },
+  data() {
+    return {
+      componenteAtivo: "PaginaSobre",
+    };
+  },
+};
+</script>
+
+<style>
+.v-enter-active {
+  transition: all 0.3s;
+}
+
+.v-enter {
+  opacity: 0;
+  transform: translate3d(0, -20px, 0);
+}
+</style>
